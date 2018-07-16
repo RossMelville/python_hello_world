fc95b2e41a12c3bc5ed3e506cc76b92276035e5d
Ross Melville <rossmelville@hotmail.com>
2018-07-16 15:56:16 +0100

    Concatenating strings

--- hello_world.py
+++ hello_world.py
@@ -6,3 +6,10 @@ print (message)
 
 name = "ross melville"
 print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "victoria"
+last_name = "scordecchia"
+full_name = first_name + " " + last_name
+print(full_name.title())
