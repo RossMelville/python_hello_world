a21a27e2fe6dd2b7ada4297f5056404b4c8ae42a
Ross Melville <rossmelville@hotmail.com>
2018-07-16 16:03:25 +0100

    Additional practice of strings

--- hello_world.py
+++ hello_world.py
@@ -13,3 +13,8 @@ first_name = "victoria"
 last_name = "scordecchia"
 full_name = first_name + " " + last_name
 print(full_name.title())
+
+print("Hello " + full_name.title() + "!")
+
+message = "Hello " + full_name.title() + " & " + name.title()
+print(message)
